e8debaeb397b88ac37d8cdc174694690905e5d1a
Mitchell Massey <mitchelltmassey@gmail.com>
2015-02-09 15:22:03 -0600

    Test deck function

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ number_of_cards = 5
 if number_of_players>3:
 number_of_cards = 4
 
-cards = [[0,0]]*number_of_cards        ## creates list, each item represents a card with another list [0,0]
+##cards = [[0,0]]*number_of_cards        ## creates list, each item represents a card with another list [0,0]
                                        ##where the first number is the number of the card and the second is the color'''
 
+## MM: I think it will be easier to create the deck before dealing
+
 def deck():
-	numbers = range(1,6)
+	numbers = range(1,6) 
 	colors = ["blue", "green", "pink", "white", "yellow"]     
 	cards = [[j for j in numbers] for i in colors]
-	print cards
+	print cards # used for test only
 
 deck()
+
+
 	
 
