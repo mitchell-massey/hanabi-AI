b7ea67fc51f0b9710be274495dbc40669c7583d0
Ben <benlewis96@gmail.com>
2015-02-09 16:55:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,26 @@ hand_of_cards = [unknown_card]*number_of_cards          ## creates the Simon's h
                                                                                                
 
 
+table = [0,0,0,0,0]                                     ## shows value of top card for each firework: [Blue,Green,Red,White,Yellow] 
+known_card_properties = [[0,0],[0,0],[0,0],[0,0],[0,0]] ## represents known number and then color of any cards
+
+
+def known_cards_check:                                  ##checks if any cards are known
+    for x in hand_of_cards:
+        for y in x:
+            if y.count(True) == 1:
+                known_card_properties[x] = y.index(True)
+
+            
+def check_for_play():                                   ##checks to see if a card in hand is playable
+    
+
+    
+
+def play_card():
+    
+
+
 def discard_card(cards_temp):
     discarded = cards_temp.pop(0)               ## takes card at index 0 out of hand_of_cards and names it discarded
     cards_temp.append(unknown_card)             ## Adds new card to index 4 in the unknown state(all values are true)
