dc53ba445392fb2d35e3555aa2163b550bd39d8f
Ben <benlewis96@gmail.com>
2015-02-09 15:38:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ number_of_cards = 5
 if number_of_players>3:
     number_of_cards = 4
 
-unknown_card = [[True]*5,[True]*5]                      ## A card is represented by a list containing two lists of five boolean values each where true means that the card is possibly that value: [[1,2,3,4,5],[Blue,Green,Red,White,Yellow]]
-hand_of_cards = [unknown_card]*number_of_cards       ## creates a hand which is a list of cards
+unknown_card = [[True]*5,[True]*5]                      ## A card is represented by a list containing two lists of five boolean
+                                                        ## values each where true means that the card is possibly that value:
+                                                        ## [[1,2,3,4,5],[Blue,Green,Red,White,Yellow]]
+
+hand_of_cards = [unknown_card]*number_of_cards          ## creates the Simon's hand which is a list of cards
                                                                                                
+
 
 def discard_card(cards_temp):
     discarded = cards_temp.pop(0)               ## takes card at index 0 out of hand_of_cards and names it discarded
-                             ## Adds new card to index 4 in the unknown state(all values are true)
+    cards_temp.append(unknown_card)             ## Adds new card to index 4 in the unknown state(all values are true)
     return cards_temp, discarded
