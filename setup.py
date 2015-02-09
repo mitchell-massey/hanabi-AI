cf8c910709489e89d9a79ae42a657c728ee57183
Ben <benlewis96@gmail.com>
2015-02-09 15:01:38 -0600

    Update setup.py
    commented and deleted print

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # Game Setup
 
-number_of_players = 3
+number_of_players = 3                   ## we may want to have the game ask for number of players at beginning
 number_of_cards = 5
 
 if number_of_players>3:
 number_of_cards = 4
 
-cards = [[0,0]]*number_of_cards
-
-print(cards)
+cards = [[0,0]]*number_of_cards        ## creates list, each item represents a card with another list [0,0]
+                                       ##where the first number is the number of the card and the second is the color'''
