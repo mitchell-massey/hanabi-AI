b5ec6d1f66d598de7e5cdb8f25acef4b0204e647
Mitchell Massey <mitchelltmassey@gmail.com>
2015-02-09 15:56:41 -0600

    Clean up function for deal / shuffle

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 # Game Setup
 
 number_of_players = 3                   ## we may want to have the game ask for number of players at beginning
-number_of_cards = 5
+total_cards = 50
 
-if number_of_players>3:
-number_of_cards = 4
+if number_of_players > 3:
+	hand_count = 4
 
-##cards = [[0,0]]*number_of_cards        ## creates list, each item represents a card with another list [0,0]
-                                       ##where the first number is the number of the card and the second is the color'''
 
-## MM: I think it will be easier to create the deck before dealing
+numbers = [1,1,1,2,2,3,3,4,4,5] 
+colors = ["blue", "green", "pink", "white", "yellow"]     
+deck = [[[j,i] for j in numbers] for i in colors]
 
-def deck():
-	numbers = range(1,6) 
-	colors = ["blue", "green", "pink", "white", "yellow"]     
-	cards = [[j for j in numbers] for i in colors]
-	print cards # used for test only
 
-deck()
+from random import shuffle
+
+shuffle(deck)
+
+print(deck)
+
+def deal():
+
+
+
 
 
 	
