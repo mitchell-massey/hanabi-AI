e9b084842e0c2823b6daa01108e69b98d5653758
Ben <benlewis96@gmail.com>
2015-02-09 15:31:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ number_of_players = 3                   ## we may want to have the game ask for 
 number_of_cards = 5
 
 if number_of_players>3:
-number_of_cards = 4
+    number_of_cards = 4
 
-cards = [[0,0]]*number_of_cards        ## creates list, each item represents a card with another list [0,0]
-                                       ##where the first number is the number of the card and the second is the color'''
+unknown_card = [[True]*5,[True]*5]                      ## A card is represented by a list containing two lists of five boolean values each where true means that the card is possibly that value: [[1,2,3,4,5],[Blue,Green,Red,White,Yellow]]
+hand_of_cards = [unknown_card]*number_of_cards       ## creates a hand which is a list of cards
+                                                                                                ## where the first number is the number of the card and the second is the color'''
+
+
+def discard_card(cards_temp):
+    discarded = cards_temp.pop(0)               ## takes card at index 0 out of hand_of_cards and names it discarded
+                             ## Adds new card to index 4 in the unknown state(all values are true)
+    return cards_temp, discarded
