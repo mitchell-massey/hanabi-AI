9f781010d5456604f71377c207447c99e55cd6f4
Ben <benlewis96@gmail.com>
2015-02-10 13:14:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ number_of_cards = 5
 if number_of_players>3:
     number_of_cards = 4
 
-unknown_card = [[True]*5,[True]*5]                      ##A card is represented by a list containing two lists of five boolean
-                                                        ##values each where true means that the card is possibly that value:
-                                                        ##[[1,2,3,4,5],[Blue,Green,Red,White,Yellow]]
+unknown_card = [[True]*5,[True]*5]                      ## A card is represented by a list containing two lists of five boolean
+                                                        ## values each where true means that the card is possibly that value:
+                                                        ## [[1,2,3,4,5],[Blue,Green,Red,White,Yellow]]
 
-hand_of_cards = [unknown_card]*number_of_cards          ##creates the Simon's hand which is a list of cards
+hand_of_cards = [unknown_card]*number_of_cards          ## creates the Simon's hand which is a list of cards
                                                                                                
 
-table = [0,0,0,0,0]                                     ##shows value of top card for each firework: [Blue,Green,Red,White,Yellow] 
-known_card_properties = [[0,0],[0,0],[0,0],[0,0],[0,0]] ##represents known number and then color of any cards
+table = [0,0,0,0,0]                                     ## shows value of top card for each firework: [Blue,Green,Red,White,Yellow] 
+known_card_properties = [[0,0],[0,0],[0,0],[0,0],[0,0]] ## represents known number and then color of any cards
 
 
 def known_cards_check(hand_of_cards,known_card_properties): ##checks if any cards are known, updates known card properties
@@ -38,6 +38,6 @@ def known_cards_check(hand_of_cards,known_card_properties): ##checks if any card
 
 
 def discard_card(cards_temp):
-    discarded = cards_temp.pop(0)               ##takes card at index 0 out of hand_of_cards and names it discarded
-    cards_temp.append(unknown_card)             ##Adds new card to index 4 in the unknown state(all values are true)
+    discarded = cards_temp.pop(0)               ## takes card at index 0 out of hand_of_cards and names it discarded
+    cards_temp.append(unknown_card)             ## Adds new card to index 4 in the unknown state(all values are true)
     return cards_temp, discarded
